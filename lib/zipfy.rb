ddc2bcf3a67603e3ee64b5b16a199290691630f9
bjubes <brian2386@gmail.com>
2017-01-01 23:15:12 -0500

    use WordData class instead of array

--- lib/zipfy.rb
+++ lib/zipfy.rb
@@ -39,15 +39,12 @@ module Zipfy
                 @total += 1
             end
             temp_hash.keys.each do |k|
-                WordData.new(k, temp_hash[k]) >> @distribution
+                @distribution << WordData.new(k, temp_hash[k])
             end
-
-            binding.pry
-
         end
 
         def sort_distribution
-            @distribution = @distribution.sort_by {|v| v[1].to_i}
+            @distribution = @distribution.sort_by {|wd| wd.frequency.to_i}
         end
 
         def calculate_zipfness
@@ -59,17 +56,16 @@ module Zipfy
             length = @distribution.length
 
             @distribution.map do |d|
-                d[2] = z * d[1]
+               d.zip_number = z * d.frequency
             end
-            binding.pry
         end
 
         def puts_distribution
             underline " "*6 + "Word Distirbution" + " "*6
             length = @distribution.length
-            @distribution.each_with_index do |w, i|
-                tabs = 4 - (w[0].length / 8).floor
-                puts w[0] + (TAB * tabs).to_s + w[1].to_s + TAB + (length - i).to_s + TAB + w[2].to_s
+            @distribution.each_with_index do |wd, i|
+                tabs = 4 - (wd.word.length / 8).floor
+                puts wd.word + (TAB * tabs).to_s + wd.frequency.to_s + TAB + (length - i).to_s + TAB + wd.zip_number.to_s
             end
         end
 
@@ -81,7 +77,6 @@ module Zipfy
         end
 
         def calculate_zipf_constant
-            most_common_word_frequency = @distribution[@distribution.keys.last]
             #this word has rank =1 , so its zipf factor should be 1/n = 1/1 = 1
             # so find its percentage and then multiply it up to one.
 
@@ -90,7 +85,7 @@ module Zipfy
             #by adding a constant to the right side
             # where constant = total words/ freq of most common word
 
-            @zipf_constant = @total.to_f / most_common_word_frequency.to_f
+            @zipf_constant = @total.to_f / @distribution.last.frequency.to_f
             @zipf_constant
         end
 
@@ -100,16 +95,14 @@ module Zipfy
             length = @distribution.length
             deviations = []
 
-            @distribution.each_with_index do |d,i|
+            @distribution.each_with_index do |wd,i|
                 theoretical = 1/(length - i).to_f
-                actual = d[2]
+                actual = wd.zip_number
                 deviation = (theoretical - actual).abs
                 deviations << deviation
             end
             sum = 0.0
-            deviations.each do |d|
-                sum += d
-            end
+            deviations.each {|d| sum += d}
             (sum/length.to_f)
          end
     end
--- lib/zipfy/word_data.rb
+++ lib/zipfy/word_data.rb
@@ -1,14 +1,13 @@
-require 'zipfy'
 module Zipfy
 	class WordData
 
 		attr_accessor :word
-		attr_accessor :freqency
+		attr_accessor :frequency
 		attr_accessor :zip_number # theoretically equal to 1/rank in a text
-	end
 
-	def initialize word, frequency = 1
-		@word = word
-		@freqency = frequency
+		def initialize word, frequency = 1
+			@word = word
+			@frequency = frequency
+		end
 	end
 end
